d802c34e8b15a3ee7e75e15715b3aa83beca3090
Terry Ott <Terry.Ott@gmail.com>
2026-04-19 17:47:48 +0000

    Using Authlib instead of pyjwt for native compiled goodness

--- endpoints/midpoint-federated-oauth-callbacks/src/federated_idp/google.py
+++ endpoints/midpoint-federated-oauth-callbacks/src/federated_idp/google.py
@@ -1,10 +1,8 @@
-import base64
 import json
 import logging
 import typing
-import urllib.parse
-import jwt
-import jwt.algorithms
+import uuid
+import authlib.jose
 import requests
 
 _logger: logging.Logger = logging.getLogger(__name__)
@@ -44,47 +42,67 @@ def oauth_callback_entry_point(event, _context):
 
     body: str = event['body']
 
-    _logger.info(f"Got request body: {body}")
+    _logger.debug(f"Got request body: {body}")
+    try:
+        parsed_body: dict[str, str] = json.loads(body)
+    except json.decoder.JSONDecodeError:
+        return _create_lambda_function_response(
+            400,
+            {
+                "error": "Body sent to callback is not a valid JSON object",
+            }
+        )
 
-    # Let's b64 decode as they say
-    decode_body: str = base64.b64decode(body).decode('utf-8')
-    _logger.info(f"Base64 decoded body: {decode_body}")
+    if not isinstance(parsed_body, dict) or len(parsed_body) != 1 or 'id_token' not in parsed_body:
+        return _create_lambda_function_response(
+            400,
+            {
+                "error": "JSON body needs to be a dict with exactly one key 'id_token'",
+            }
+        )
 
-    # parse_qs returns lists of strings because headers can be duplicated, but we ignore that and
-    #   take the first assuming no dupes
-    parsed_dict_from_body: dict[str, str] = {k: v[0] for k, v in urllib.parse.parse_qs(decode_body).items()}
-
-    # return _create_lambda_function_response(
-    #     200,
-    #     parsed_dict_from_body,
-    # )
-
-    id_token: str = parsed_dict_from_body['credential']
+    id_token: str = parsed_body['id_token']
 
     # 1. Get Google's Public Keys (JWKS)
     jwks_url = "https://www.googleapis.com/oauth2/v3/certs"
     response = requests.get(jwks_url)
     jwks = response.json()
 
-    # 2. Extract Key ID (kid) from token header
-    unverified_header = jwt.get_unverified_header(id_token)
-    kid = unverified_header['kid']
-
-    # 3. Find the matching key
-    key_data = next(k for k in jwks['keys'] if k['kid'] == kid)
-    # So we can check state against original request -- but won't
-
     # Now we take the code and exchange it for ID/access tokens
+    try:
+        validated_claims: dict[str, typing.Any] = jwt.decode(
+            id_token,
+            jwt.algorithms.RSAAlgorithm.from_jwk(key_data),
+            algorithms=["RS256"],
+            audience="263659947191-e0sr8qg2pmofgb15h5lc1ihu7bhni26j.apps.googleusercontent.com"
+        )
+    except jwt.InvalidTokenError:
+        _logger.warning(f"Invalid token passed in body, rejecting login: {id_token}")
+        return _create_lambda_function_response(
+            401,
+            {
+                "error": "Provided token failed signature check",
+            }
+        )
 
-    # Fake that out and return signed JWT ID and access tokens
+    _logger.info("ID token is valid with claims:")
+    _logger.info(validated_claims)
+
+    assigned_session_id: str = str(uuid.uuid4())
+    _logger.info(f"Assigned session ID: {assigned_session_id}")
+
+    cookie_value: str = f"session_id={assigned_session_id}; HttpOnly; Secure; SameSite=None; Path=/; Max-Age=3600"
+
     return _create_lambda_function_response(
         200,
         {
-            "decoded_token": jwt.decode(
-                id_token,
-                jwt.algorithms.RSAAlgorithm.from_jwk(key_data),
-                algorithms=["RS256"],
-                audience="263659947191-e0sr8qg2pmofgb15h5lc1ihu7bhni26j.apps.googleusercontent.com"
-            )
+            "login_status"                      : "SUCCESS",
         },
+
+        # Hide the cookie from JS, all JS knows is API calls start working, can't see why
+        headers={
+            "Set-Cookie"                        : cookie_value,
+            "Access-Control-Allow-Credentials"  : "true",
+            "Access-Control-Allow-Origin"       : "https://midpoint-ui-oidc.pages.dev/sign_up"
+        }
     )
